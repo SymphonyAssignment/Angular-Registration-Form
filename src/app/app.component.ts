be6fa24359897f8f5277129691d396c325bc2f8e
Rajesh Swain <rajesh.swain@symphonytalent.com>
2020-09-07 10:38:36 +0530

    Frontend connected with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { ConstantPool } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {UsersService} from './users.service'
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn:'root'
 })
@@ -15,20 +17,48 @@ import {UsersService} from './users.service'
 })
 export class AppComponent {
   
-  constructor(private userService:UsersService){}
+  toggleShowHide: string = "block";  
+  thankyouShowHide: string= "none";
+
+  constructor(private userService:UsersService,private router: Router){}
+
 
   educations=['Btech','Mtech','MCA','MBA'];
   cnfPasswordError= true;
+  emailExist=false;
   dobError=true;
-  userModel = new User('Ehraz','','','xyz@gmail.com',null,'','','',true);
+  userModel = new User('','','','',null,'','','',true);
+ 
 
   onSubmit(){
-      this.userService.register(this.userModel)
-      .subscribe(
-        data => console.log('Success',data),
-        error => console.log('error',error)
-      )
+    this.userService.register(this.userModel)
+    .subscribe(
+      data => {
+        if(this.emailExists(data))
+      {
+        this.thankyouShowHide= "block";
+        this.toggleShowHide="none";
+      }
+      },
+      error => console.log('error',error)
+    )
+}
+
+
+emailExists(data){
+  if(data==1){
+    this.emailExist=false;
+    console.log("Candidate Registered");
+    return true;
   }
+  else{
+    this.emailExist=true;
+    console.log("Email is already present in");
+    return false;
+  }
+}
+
+
 
   validatePassword(value,value1){
     if(value=== value1)
@@ -38,6 +68,8 @@ export class AppComponent {
     console.log(this.cnfPasswordError);
 
   }
+   
+ 
 
 
   validatedob(value)
@@ -57,18 +89,6 @@ export class AppComponent {
     console.log(this.dobError);
     }
   }
-
-
-  f1(){
-    fetch('http://localhost:8080/Regform/rest/candidate')
-            .then(result => {
-                 console.log(result);
-            })
-            .catch(error => console.log(error));
-    
-    }
-    
-  
 
 
 }
